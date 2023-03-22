5e3c9e9626e16a4314f052f9e849df30cb9bae97
Alex <alextortosa2001@gmail.com>
2023-03-22 12:15:18 +0100

    push final

--- functions.py
+++ functions.py
@@ -1,66 +1,72 @@
 import math
 
 def read_data(fichero):
-    f = open(fichero,mode="rt", encoding="utf-8")
-    linea = f.readline()
-    cont = 1
+    try:
+        f = open(fichero,mode="rt", encoding="utf-8")
+        linea = f.readline()
+        cont = 1
     
-    diccionariobase = {}
-    diccionario = {}
+        diccionariobase = {}
+        diccionario = {}
 
-    for i in linea.split(","):
-        i = i.replace('\n', '')
-        diccionariobase[i] = ""
-    linea = f.readline()
-
-    while linea != "":
-        diccionario["dato"+str(cont)] = diccionariobase.copy()
-        
-        contador = 0
         for i in linea.split(","):
             i = i.replace('\n', '')
-            diccionario["dato"+str(cont)][list(diccionariobase.keys())[contador]] = i
-            contador += 1
-        cont += 1
+            diccionariobase[i] = ""
         linea = f.readline()
+
+        while linea != "":
+            diccionario["dato"+str(cont)] = diccionariobase.copy()
+            
+            contador = 0
+            for i in linea.split(","):
+                i = i.replace('\n', '')
+                diccionario["dato"+str(cont)][list(diccionariobase.keys())[contador]] = i
+                contador += 1
+            cont += 1
+            linea = f.readline()
+    except Exception as e:
+        print("Ha ocurrido la excepcion: "+e)
 
     return diccionario
 
 def split(diccionario):
-    dic1 = {}
-    dic2 = {}
-    for i in diccionario:
+    try:
+        dic1 = {}
+        dic2 = {}
+        for i in diccionario:
 
-        if diccionario[i]["type"] == "white":
-            del diccionario[i]["type"]
-            dic1[i]=diccionario[i]
-        else: 
-            del diccionario[i]["type"]
-            dic2[i]=diccionario[i]
-
+            if diccionario[i]["type"] == "white":
+                del diccionario[i]["type"]
+                dic1[i]=diccionario[i]
+            else: 
+                del diccionario[i]["type"]
+                dic2[i]=diccionario[i]
+    except Exception as e:
+        print("Ha ocurrido la excepcion: "+e)
     return dic1, dic2
 
 def reduce(diccionario, atributo):
-    lista = []
-    for i in diccionario:
+    try:
 
-        lista.append(diccionario[i][atributo])
+        lista = []
+        for i in diccionario:
+
+            lista.append(diccionario[i][atributo])
+    except Exception as e:
+        print("Ha ocurrido la excepcion: "+e)
     return lista 
 
 def silhouette(lista1, lista2):
-    sil = 0.0
-    ai=[]
-    bi=[]
-    for i in lista1:
-        for j in lista2:
-            ai.append(math.sqrt(math.pow(abs(float(i)-float(j)),2)))
-            bi.append(math.sqrt(math.pow(abs((sum(lista1)/len(lista2))-j))))
-    sil = (bi-ai)/max(ai,bi)
+    try:
+        sil = 0.0
+        ai=[]
+        bi=[]
+        for i in lista1:
+            for j in lista2:
+                ai.append(math.sqrt(math.pow(abs(float(i)-float(j)),2)))
+                # no terminada
+                bi.append(math.sqrt(math.pow(abs((sum(lista1)/len(lista1))-float(j)),2)))
+        sil = (bi-ai)/max(ai,bi)
+    except Exception as e:
+        print("Ha ocurrido la excepcion: "+e)
     return sil
-
-
-diccionario = read_data("winequality.csv")
-dicW,dicR = split(diccionario)
-list1=reduce(dicW, "fixed acidity")
-list2=reduce(dicW, "alcohol")
-print(silhouette(list1,list2))
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import functions
+
+diccionario = functions.read_data("winequality.csv")
+dicW,dicR = functions.split(diccionario)
+list1=functions.reduce(dicW, "fixed acidity")
+list2=functions.reduce(dicW, "alcohol")
