ed9a93dafa8d60a5edae05aee589f03a58b2dc2b
Alex <alextortosa2001@gmail.com>
2023-03-22 10:30:35 +0100

    checkpoint 2

--- functions.py
+++ functions.py
@@ -1,10 +1,27 @@
 def read_data(fichero):
     f = open(fichero,mode="rt", encoding="utf-8")
     linea = f.readline()
+    cont = 1
+    diccionariobase = {}
     diccionario = {}
-    while linea != "":
-        for i in linea.split(","):
-            
-        linea = f.readline()
 
-    return palabras
+    
+
+    for i in linea.split(","):
+        diccionariobase[i] = ""
+
+    
+
+    while linea != "":
+        diccionario["dato"+str(cont)] = diccionariobase.copy()
+
+        
+    #     for i in linea.split(","):
+    #         diccionario[i] = i
+    #     linea = f.readline()
+    #     cont += 1
+
+    return diccionario
+
+
+print(read_data("winequality.csv"))
